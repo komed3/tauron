5f9377826cf3eee47b414ba173d32786e9b74fd9
komed3 <webmaster@komed3.de>
2026-08-05 23:53:54 +0200

    implement the `Engine::use` method

--- src/engine/engine.cpp
+++ src/engine/engine.cpp
@@ -1,3 +1,9 @@
 #include <tauron/engine/engine.hpp>
 
 #include <utility>
+
+namespace tauron {
+  void Engine::use( std::unique_ptr< Module > module ) {
+    modules.push_back( std::move( module ) );
+  }
+}
