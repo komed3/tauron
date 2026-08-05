c1ff3f374a4672146587f03136821e4e8bf40aa3
komed3 <webmaster@komed3.de>
2026-08-05 23:50:09 +0200

    declare public / private methods for engine class

--- include/tauron/engine/engine.hpp
+++ include/tauron/engine/engine.hpp
@@ -6,5 +6,12 @@
 #include <tauron/core/module.hpp>
 
 namespace tauron {
-  class Engine {};
+  class Engine {
+  public:
+    void use( std::unique_ptr< Module > module );
+    void process( Context& context );
+
+  private:
+    std::vector< std::unique_ptr< Module > > modules;
+  };
 }
