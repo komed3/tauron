e1fe6bee068a1bd2699cb28f5bb927db994d8fc1
komed3 <webmaster@komed3.de>
2026-08-06 00:09:29 +0200

    declare a template for the `Engine::use` method

--- include/tauron/engine/engine.hpp
+++ include/tauron/engine/engine.hpp
@@ -8,10 +8,17 @@
 namespace tauron {
   class Engine {
   public:
-    void use( std::unique_ptr< Module > module );
+    template< typename T, typename... Args >
+    void use( Args&&... args );
+
     void process( Context& context );
 
   private:
     std::vector< std::unique_ptr< Module > > modules;
   };
+
+  template< typename T, typename... Args >
+  void Engine::use( Args&&... args ) {
+    modules.push_back( std::make_unique< T >( std::forward< Args >( args )... ) );
+  }
 }
