59c6290aaabaad732e3014e5fb5c67a4d1633127
komed3 <webmaster@komed3.de>
2026-08-06 15:26:58 +0200

    declare the EvolutionModule

---
+++ include/tauron/modules/evolution.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "module.hpp"
+
+namespace tauron::modules {
+
+class EvolutionModule : public Module {
+public:
+    std::string_view name() const override;
+    void process( core::Context& context ) override;
+};
+
+}
