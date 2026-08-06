d7e61aae538a380ac787acfa4448026703717be8
komed3 <webmaster@komed3.de>
2026-08-06 15:31:37 +0200

    "process" evolution module

--- src/modules/evolution.cpp
+++ src/modules/evolution.cpp
@@ -9,4 +9,13 @@ std::string_view EvolutionModule::name() const {
   return "Evolution";
 }
 
+void EvolutionModule::process( core::Context& context ) {
+  context.state.counter++;
+
+  std::cout
+    << "[Evolution] counter = "
+    << context.state.counter
+    << "\n";
+}
+
 }
