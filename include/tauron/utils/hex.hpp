efc98bd60232a7b7b7c6337b7716c8cf2c7fb36e
komed3 <webmaster@komed3.de>
2026-08-06 22:45:44 +0200

    use a template for hex utility

--- include/tauron/utils/hex.hpp
+++ include/tauron/utils/hex.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
 #include <cstdint>
+#include <iomanip>
+#include <sstream>
 #include <string>
-#include <vector>
 
 namespace tauron::utils {
 
-std::string hex( const std::vector< std::uint8_t >& bytes, char sep = ' ', bool upper = false );
+template< typename Container >
+std::string hex( const Container& bytes, char separator = ' ', bool uppercase = false ) {
+  std::ostringstream stream;
+
+  if ( uppercase ) stream << std::uppercase;
+  stream << std::hex << std::setfill( '0' );
+
+  bool first = true;
+
+  for ( const auto byte : bytes ) {
+    if ( ! first && separator ) stream << separator;
+
+    stream << std::setw( 2 ) << static_cast< unsigned >( byte );
+    first = false;
+  }
+
+  return stream.str();
+};
 
 }
