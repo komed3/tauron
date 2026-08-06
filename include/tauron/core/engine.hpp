ad332b4ff3e9c10e862406d8b3068b9260358d45
komed3 <webmaster@komed3.de>
2026-08-06 15:20:48 +0200

    declare Engine interface

--- include/tauron/core/engine.hpp
+++ include/tauron/core/engine.hpp
@@ -3,4 +3,15 @@
 #include "tauron/core/context.hpp"
 #include "tauron/pipeline/pipeline.hpp"
 
-namespace tauron::core {}
+namespace tauron::core {
+
+class Engine {
+public:
+  void run();
+
+private:
+  Context context;
+  pipeline::Pipeline pipeline;
+};
+
+}
