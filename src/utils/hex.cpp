da47606b2fa8aabc5f5bdf6a92498ac3710eff4d
komed3 <webmaster@komed3.de>
2026-08-06 20:37:15 +0200

    implement byte decoder

--- src/utils/hex.cpp
+++ src/utils/hex.cpp
@@ -1,9 +1,20 @@
 #include "tauron/utils/hex.hpp"
 
+#include <iomanip>
+#include <sstream>
+
 namespace tauron::utils {
 
 std::string hex( const std::vector< std::uint8_t >& bytes ) {
   std::ostringstream stream;
+
+  for ( const auto& byte : bytes )
+    stream
+      << std::hex
+      << std::setw( 2 )
+      << std::setfill( '0' )
+      << static_cast< int >( byte )
+      << ' ';
 }
 
 }
