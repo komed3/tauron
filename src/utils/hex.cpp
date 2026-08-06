b1104826aef817c11736ec6ee1a23e3f209eabf3
komed3 <webmaster@komed3.de>
2026-08-06 20:54:34 +0200

    Update hex.cpp

--- src/utils/hex.cpp
+++ src/utils/hex.cpp
@@ -5,16 +5,20 @@
 
 namespace tauron::utils {
 
-std::string hex( const std::vector< std::uint8_t >& bytes ) {
+std::string hex( const std::vector< std::uint8_t >& bytes, char sep, bool upper ) {
   std::ostringstream stream;
 
-  for ( auto byte : bytes )
-    stream
-      << std::hex
-      << std::setw( 2 )
-      << std::setfill( '0' )
-      << static_cast< int >( byte )
-      << ' ';
+  if ( upper ) stream << std::uppercase;
+  stream << std::hex << std::setfill( '0' );
+
+  bool first = true;
+
+  for ( auto byte : bytes ) {
+    if ( ! first && sep ) stream << sep;
+
+    stream << std::setw( 2 ) << static_cast< int >( byte );
+    first = false;
+  }
 
   return stream.str();
 }
