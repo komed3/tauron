bf6b57c16f212266fc846a3f36af933ae2792502
komed3 <webmaster@komed3.de>
2026-08-06 15:42:28 +0200

    connect modules

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -5,3 +5,12 @@
 
 #include "tauron/modules/cipher.hpp"
 #include "tauron/modules/evolution.hpp"
+
+namespace tauron::core {
+
+Engine::Engine() {
+  pipeline.add( std::make_unique< modules::EvolutionModule >() );
+  pipeline.add( std::make_unique< modules::CipherModule >() );
+}
+
+}
