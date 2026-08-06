0c59767c63e4cfd0f6f999471c1ff812f30766fb
komed3 <webmaster@komed3.de>
2026-08-06 23:28:19 +0200

    Create registry.hpp

---
+++ include/tauron/modules/registry.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "module.hpp"
+
+namespace tauron::modules {
+
+class Registry {
+};
+
+}
