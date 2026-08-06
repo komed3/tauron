fe75f7e56cf7455ed55ca41c87cc1b50532ef4a9
komed3 <webmaster@komed3.de>
2026-08-06 23:30:03 +0200

    add unordered map for modules

--- include/tauron/modules/registry.hpp
+++ include/tauron/modules/registry.hpp
@@ -14,6 +14,9 @@ public:
   using Factory = std::function< std::unique_ptr< Module >() >;
   void registerModule( const std::string& name, Factory factory );
   std::unique_ptr< Module > create( const std::string& name );
+
+private:
+  std::unordered_map< std::string, Factory > modules;
 };
 
 }
