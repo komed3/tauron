1308a01a49f9802bafc32ce3328ce9340080819c
komed3 <webmaster@komed3.de>
2026-08-06 14:56:30 +0200

    declare Profile in tauron::core

---
+++ include/tauron/core/config.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace tauron::core {
+
+enum class Profile {
+  Fast,
+  Balanced,
+  Secure,
+  Custom
+};
+
+}
--- include/tauron/core/context.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace tauron {
-  class Context {
-  public:
-    Context() = default;
-  };
-}
