2f2990fc4455ab0dcb9dd0d64a5aab9edad4685f
komed3 <webmaster@komed3.de>
2026-08-07 14:25:02 +0200

    Update engine.hpp

--- include/tauron/core/engine.hpp
+++ include/tauron/core/engine.hpp
@@ -1,7 +1,19 @@
 #pragma once
 
+#include <string_view>
+#include <vector>
+
+#include "tauron/core/config.hpp"
+#include "tauron/core/context.hpp"
+#include "tauron/modules/registry.hpp"
+#include "tauron/pipeline/pipeline.hpp"
+
 namespace tauron::core {
 
-
+class Engine {
+public:
+  explicit Engine( Config config = {} );
+  void initialize();
+};
 
 }
