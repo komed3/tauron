fcc1c0f225660da009b42674b14707ac676ff9a7
komed3 <webmaster@komed3.de>
2026-08-07 07:47:55 +0200

    Update state.hpp

--- include/tauron/core/state.hpp
+++ include/tauron/core/state.hpp
@@ -6,8 +6,10 @@
 namespace tauron::core {
 
 struct State {
-  std::array< std::uint8_t, 32 > seed {};
+  std::array< std::uint8_t, 32 > rootSeed {};
+  std::array< std::uint64_t, 4 > registers {};
   std::uint64_t counter = 0;
+  std::uint64_t epoch = 0;
 };
 
 }
