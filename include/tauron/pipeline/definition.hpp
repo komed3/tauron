14622786e294e8875b8b2e552613cc54fd89f009
komed3 <webmaster@komed3.de>
2026-08-07 11:53:44 +0200

    declare profile definitions

--- include/tauron/pipeline/definition.hpp
+++ include/tauron/pipeline/definition.hpp
@@ -5,4 +5,31 @@
 #include "tauron/core/profile.hpp"
 #include "tauron/pipeline/step.hpp"
 
-namespace tauron::pipeline {}
+namespace tauron::pipeline {
+
+inline constexpr std::array defaultProfile {
+  Step { "initialize", modules::ModuleType::Setup },
+  Step { "evolution", modules::ModuleType::Processing },
+  Step { "mix", modules::ModuleType::Processing },
+  Step { "rotate", modules::ModuleType::Processing },
+  Step { "xor", modules::ModuleType::Processing },
+  Step { "finalize", modules::ModuleType::Finalization }
+};
+
+inline constexpr std::array fastProfile {
+  Step { "initialize", modules::ModuleType::Setup },
+  Step { "evolution", modules::ModuleType::Processing },
+  Step { "xor", modules::ModuleType::Processing },
+  Step { "finalize", modules::ModuleType::Finalization }
+};
+
+inline constexpr std::array experimentalProfile {
+  Step { "initialize", modules::ModuleType::Setup },
+  Step { "evolution", modules::ModuleType::Processing, 3 },
+  Step { "mix", modules::ModuleType::Processing, 2 },
+  Step { "rotate", modules::ModuleType::Processing },
+  Step { "xor", modules::ModuleType::Processing },
+  Step { "finalize", modules::ModuleType::Finalization }
+};
+
+}
