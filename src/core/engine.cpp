f895e93cd47e59cad470361efc68c3170b7fc4f1
komed3 <webmaster@komed3.de>
2026-08-07 14:33:14 +0200

    initialize engine modules

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -1,7 +1,23 @@
 #include "tauron/core/engine.hpp"
+#include "tauron/modules/evolution.hpp"
+#include "tauron/modules/finalize.hpp"
+#include "tauron/modules/initialize.hpp"
+#include "tauron/modules/mix.hpp"
+#include "tauron/modules/rotate.hpp"
+#include "tauron/modules/xor.hpp"
+#include "tauron/pipeline/builder.hpp"
 
 namespace tauron::core {
 
+void Engine::initialize() {
+  registry.add< modules::InitializeModule >();
+  registry.add< modules::EvolutionModule >();
+  registry.add< modules::MixModule >();
+  registry.add< modules::RotateModule >();
+  registry.add< modules::XorModule >();
+  registry.add< modules::FinalizeModule >();
 
+  pipeline = pipeline::Builder::build( config.profile, registry );
+}
 
 }
