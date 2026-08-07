edefda1908db93bd28eab8290038a3ab0ecff3c4
komed3 <webmaster@komed3.de>
2026-08-07 07:51:40 +0200

    declare structs

--- include/tauron/core/context.hpp
+++ include/tauron/core/context.hpp
@@ -6,6 +6,16 @@
 
 namespace tauron::core {
 
+struct Buffers {
+  std::vector< std::uint8_t > input;
+  std::vector< std::uint8_t > output;
+};
+
+struct Runtime {
+  std::uint64_t processedBlocks = 0;
+  std::uint64_t processedBytes = 0;
+};
+
 struct Context {
   core::State state;
   std::vector< std::uint8_t > input;
