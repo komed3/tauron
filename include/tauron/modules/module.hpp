9e5ea21244b5610373f55d65794f4eca409c3a4d
komed3 <webmaster@komed3.de>
2026-08-07 08:40:13 +0200

    add name/type virtual methods

--- include/tauron/modules/module.hpp
+++ include/tauron/modules/module.hpp
@@ -4,9 +4,17 @@
 
 namespace tauron::modules {
 
+enum class ModuleType {
+  Setup,
+  Processing,
+  Finalization
+};
+
 class Module {
 public:
   virtual ~Module() = default;
+  virtual const char* name() const = 0;
+  virtual ModuleType type() const = 0;
   virtual void execute( core::Context& context ) = 0;
 };
 
