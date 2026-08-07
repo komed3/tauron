b83512d7eda66351c1f6987c43ea8c02b3fce247
komed3 <webmaster@komed3.de>
2026-08-07 14:53:23 +0200

    write example

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+
+#include "tauron/core/engine.hpp"
+#include "tauron/utils/hex.hpp"
+
 int main() {
-  return 0;
+  tauron::core::Config config;
+  config.profile = tauron::core::Profile::Default;
+  config.debug = true;
+
+  tauron::core::Engine engine( config );
+  engine.initialize();
+
+  auto encrypted = engine.process(
+    "Hello",
+    tauron::core::Operation::Encrypt
+  );
+
+  std::cout
+    << "Encrypted: "
+    << tauron::utils::hex( encrypted )
+    << "\n";
 }
