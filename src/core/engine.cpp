f330f9fc4bd3a024bcbba16830c89fd1ad328385
komed3 <webmaster@komed3.de>
2026-08-07 14:38:08 +0200

    process bytes

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -20,4 +20,15 @@ void Engine::initialize() {
   pipeline = pipeline::Builder::build( config.profile, registry );
 }
 
+std::vector< std::uint8_t > Engine::process( std::string_view input, Operation operation ) {
+  Context context;
+  context.config = config;
+  context.operation = operation;
+
+  context.buffers.input.assign( input.begin(), input.end() );
+  pipeline.run( context );
+
+  return std::move( context.buffers.output );
+}
+
 }
