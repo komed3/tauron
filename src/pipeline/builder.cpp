77e39f3986835bf2078dcb9a3a66b3fbfe7691c0
komed3 <webmaster@komed3.de>
2026-08-07 12:48:15 +0200

    Create builder.cpp

--- src/pipeline/builder.cpp
+++ src/pipeline/builder.cpp
@@ -1,7 +1,12 @@
 #include "tauron/pipeline/builder.hpp"
+#include "tauron/pipeline/definition.hpp"
 
 namespace tauron::pipeline {
 
+Pipeline build( core::Profile profile, modules::Registry registry ) {
+  Pipeline pipeline;
 
+  return pipeline;
+}
 
 }
