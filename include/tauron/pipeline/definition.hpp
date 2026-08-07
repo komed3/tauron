21b33560350e560b42848205d56d541548715a55
komed3 <webmaster@komed3.de>
2026-08-07 11:49:36 +0200

    declare pipeline definition file

--- include/tauron/pipeline/definition.hpp
+++ include/tauron/pipeline/definition.hpp
@@ -1,16 +1,8 @@
 #pragma once
 
-#include <string_view>
-#include <cstddef>
+#include <array>
 
-#include "tauron/modules/module.hpp"
+#include "tauron/core/profile.hpp"
+#include "tauron/pipeline/step.hpp"
 
-namespace tauron::pipeline {
-
-struct Step {
-  std::string_view module;
-  modules::ModuleType type;
-  std::size_t repeat = 1;
-};
-
-}
+namespace tauron::pipeline {}
