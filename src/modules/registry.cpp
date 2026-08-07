788d863f3376ec26bcb81bd7edc985a6db84cffe
komed3 <webmaster@komed3.de>
2026-08-07 09:03:54 +0200

    implement method to get all module names from registry

--- src/modules/registry.cpp
+++ src/modules/registry.cpp
@@ -15,6 +15,14 @@ std::size_t Registry::size() const {
   return factories.size();
 }
 
+std::vector< std::string > Registry::names() const {
+  std::vector< std::string > result;
+  result.reserve( this->size() );
+
+  for ( const auto& [ name, _ ] : factories ) result.push_back( name );
+  return result;
+}
+
 const ModuleFactory* Registry::find ( const std::string& name ) const {
   auto iterator = factories.find( name );
 
