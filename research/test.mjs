6b9712055cea6f2ea17764e4e589430413379565
komed3 <webmaster@komed3.de>
2026-08-08 20:45:02 +0200

    flip bits

--- research/test.mjs
+++ research/test.mjs
@@ -1,18 +1,4 @@
-import { expandKey_v1 as algo } from './expandKey_v1.mjs';
-import { KEY_SIZE, outDiffer, outKeys } from './utils.mjs';
+import { expandKey_v2 } from './expandKey_v2.mjs';
+import { runTests } from './testbench.mjs';
 
-const key1 = new Uint8Array( KEY_SIZE );
-const shed1 = algo( key1 );
-
-console.log( '=== SHEDULE 1 ===' );
-outKeys( shed1 );
-
-const key2 = new Uint8Array( KEY_SIZE );
-key2[ KEY_SIZE - 1 ] = 1;
-const shed2 = algo( key2 );
-
-console.log( '=== SHEDULE 2 ===' );
-outKeys( shed2 );
-
-console.log( '=== DIFFER ===' );
-outDiffer( shed1, shed2 );
+runTests( expandKey_v2 );
