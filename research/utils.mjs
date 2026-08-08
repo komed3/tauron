53e56564e4bb6a0606663e4b5c1bec1d37c0664c
komed3 <webmaster@komed3.de>
2026-08-09 00:05:32 +0200

    Update utils.mjs

--- research/utils.mjs
+++ research/utils.mjs
@@ -20,14 +20,14 @@ export const hammingDistance = ( a, b ) => {
   return bits;
 };
 
-const randomKey = () => {
+export const randomKey = () => {
   const key = new Uint8Array( KEY_SIZE );
   webcrypto.getRandomValues( key );
 
   return key;
 };
 
-const cloneKey = ( key ) => new Uint8Array( key );
+export const cloneKey = ( key ) => new Uint8Array( key );
 
 export const schedule = ( key, fn ) => {
   const keys = [ new Uint8Array( key ) ];
