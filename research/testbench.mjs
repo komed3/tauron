0581636542f85b4c44e9d8283a1c9eccf12fe448
komed3 <webmaster@komed3.de>
2026-08-08 20:46:04 +0200

    implement equalBytes() helper method

--- research/testbench.mjs
+++ research/testbench.mjs
@@ -23,3 +23,10 @@ const byteDistance = ( a, b ) => {
 
   return bytes;
 };
+
+const equalBytes = ( a, b ) => {
+  if ( a.length !== b.length ) return false;
+  for ( let i = 0; i < a.length; i++ ) if ( a[ i ] !== b[ i ] ) return false;
+
+  return true;
+};
