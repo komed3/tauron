b78cebc968d167ddbe9c93187ee8ddbfd71b5901
komed3 <webmaster@komed3.de>
2026-08-08 14:06:43 +0200

    implement average, deviation and det. test

--- research/statistical.mjs
+++ research/statistical.mjs
@@ -8,19 +8,18 @@ const randomKey = () => {
   return key;
 };
 
-const incrementKey = key => {
+const incrementKey = ( key ) => {
   const result = new Uint8Array( key );
 
   for ( let i = result.length - 1; i >= 0; i-- ) {
     result[ i ]++;
-
     if ( result[ i ] !== 0 ) break;
   }
 
   return result;
 };
 
-const countBits = byte => {
+const countBits = ( byte ) => {
   let bits = 0;
 
   while ( byte ) {
@@ -29,4 +28,21 @@ const countBits = byte => {
   }
 
   return bits;
+};
+
+const average = ( values ) => values.reduce( ( sum, value ) => sum + value, 0 ) / values.length;
+
+const deviation = ( values ) => {
+  const avg = average( values );
+  return Math.sqrt( average( values.map( value => ( value - avg ) ** 2 ) ) );
+};
+
+const testDeterminism = ( algo ) => {
+  const key = randomKey();
+
+  const a = algo( key );
+  const b = algo( key );
+
+  const identical = a.every( ( value, index ) => value === b[ index ] );
+  console.log( `Determinism       ${ identical ? 'PASS' : 'FAIL' }` );
 };
