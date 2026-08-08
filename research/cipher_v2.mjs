d7c9dfe8f8c7dbea1a6e83393d4ddfa543c1f3f1
komed3 <webmaster@komed3.de>
2026-08-09 01:21:51 +0200

    Create cipher_v2.mjs

---
+++ research/cipher_v2.mjs
@@ -0,0 +1,16 @@
+import { expandKey_v2 } from './expandKey_v2.mjs';
+
+const BLOCK_SIZE = 32;
+
+const permutation = [
+  0, 13, 26, 7, 20, 1, 14, 27,
+  8, 21, 2, 15, 28, 9, 22, 3,
+  16, 29, 10, 23, 4, 17, 30, 11,
+  24, 5, 18, 31, 12, 19, 6, 25
+];
+
+const inversePermutation = new Uint8Array( BLOCK_SIZE );
+for ( let i = 0; i < BLOCK_SIZE; i++ ) inversePermutation[ permutation[ i ] ] = i;
+
+const rotl = ( value, bits ) => ( ( value << bits ) | ( value >>> ( 32 - bits ) ) ) >>> 0;
+const rotr = ( value, bits ) => ( ( value >>> bits ) | ( value << ( 32 - bits ) ) ) >>> 0;
