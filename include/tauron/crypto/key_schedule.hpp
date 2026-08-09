fc6640ec52110d4ea0a4af2a807588473666c96a
komed3 <webmaster@komed3.de>
2026-08-09 17:07:19 +0200

    declare min/max rounds

--- include/tauron/crypto/key_schedule.hpp
+++ include/tauron/crypto/key_schedule.hpp
@@ -9,6 +9,9 @@ namespace tauron::crypto {
 
 inline constexpr std::size_t KEY_SIZE = 32;
 
+inline constexpr std::size_t MIN_ROUNDS = 2;
+inline constexpr std::size_t MAX_ROUNDS = 128;
+
 using Key = std::array< std::uint8_t, KEY_SIZE >;
 using RoundKeys = std::vector< Key >;
 
