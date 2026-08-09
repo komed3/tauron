7a1700b7888af582b52bf35bb05bffbbda41a2ec
komed3 <webmaster@komed3.de>
2026-08-10 01:41:00 +0200

    min fix

--- include/tauron/crypto/words.hpp
+++ include/tauron/crypto/words.hpp
@@ -8,31 +8,29 @@
 
 namespace tauron::crypto {
 
-inline constexpr std::size_t WORDS = 8;
-
 using Words = std::array< std::uint32_t, WORDS >;
 using Bytes = std::array< std::uint8_t, BLOCK_SIZE >;
 
-constexpr std::uint32_t toWord( const Bytes& bytes, std::size_t offset ) noexcept {
+inline constexpr std::uint32_t toWord( const Bytes& bytes, std::size_t offset ) noexcept {
   return static_cast< std::uint32_t >( bytes[ offset ] )
     | ( static_cast< std::uint32_t >( bytes[ offset + 1 ] ) <<  8 )
     | ( static_cast< std::uint32_t >( bytes[ offset + 2 ] ) << 16 )
     | ( static_cast< std::uint32_t >( bytes[ offset + 3 ] ) << 24 );
 }
 
-void toWords( const Bytes& bytes, Words& words ) noexcept {
+inline void toWords( const Bytes& bytes, Words& words ) noexcept {
   for ( std::size_t i = 0; i < words.size(); ++i )
     words[ i ] = toWord( bytes, i * 4 );
 }
 
-constexpr void fromWord( std::uint32_t word, Bytes& bytes, std::size_t offset ) noexcept {
+inline constexpr void fromWord( std::uint32_t word, Bytes& bytes, std::size_t offset ) noexcept {
   bytes[ offset ] = static_cast< std::uint8_t >( word );
   bytes[ offset + 1 ] = static_cast< std::uint8_t >( word >>  8 );
   bytes[ offset + 2 ] = static_cast< std::uint8_t >( word >> 16 );
   bytes[ offset + 3 ] = static_cast< std::uint8_t >( word >> 24 );
 }
 
-void fromWords( const Words& words, Bytes& bytes ) noexcept {
+inline void fromWords( const Words& words, Bytes& bytes ) noexcept {
   for ( std::size_t i = 0; i < words.size(); ++i )
     fromWord( words[ i ], bytes, i * 4 );
 }
