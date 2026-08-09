a3e195b8fea694ac0b08da53212bfe737eebfe8e
komed3 <webmaster@komed3.de>
2026-08-09 14:19:31 +0200

    use CLI for filename

--- research/file_check.mjs
+++ research/file_check.mjs
@@ -3,3 +3,13 @@ import { createHash } from 'node:crypto';
 import { basename, dirname, join } from 'node:path';
 
 import { encrypt, decrypt } from './tauron_v2.mjs';
+
+
+const passphrase = 'Tauron file test';
+
+const input = process.argv[ 2 ];
+
+if ( ! input ) {
+  console.error( 'Usage: node file_check.mjs <file>' );
+  process.exit( 1 );
+}
