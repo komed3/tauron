3d4c72dcaeae8457fd8512c8cd1cd993a0e6a008
komed3 <webmaster@komed3.de>
2026-08-10 01:41:12 +0200

    Update constants.hpp

--- include/tauron/crypto/constants.hpp
+++ include/tauron/crypto/constants.hpp
@@ -10,4 +10,6 @@ inline constexpr std::size_t KEY_SIZE = 32;
 inline constexpr std::size_t MIN_ROUNDS = 2;
 inline constexpr std::size_t MAX_ROUNDS = 128;
 
+inline constexpr std::size_t WORDS = 8;
+
 }
