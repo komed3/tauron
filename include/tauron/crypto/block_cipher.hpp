e12909dc82109e183562fda145fb1790dbc89c19
komed3 <webmaster@komed3.de>
2026-08-09 23:13:18 +0200

    declare public methods

--- include/tauron/crypto/block_cipher.hpp
+++ include/tauron/crypto/block_cipher.hpp
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <cstdint>
 
+#include "tauron/crypto/key_schedule.hpp"
+
 namespace tauron::crypto {
 
 inline constexpr std::size_t BLOCK_SIZE = 32;
@@ -12,7 +14,8 @@ using Block = std::array< std::uint8_t, BLOCK_SIZE >;
 
 class BlockCipher {
 public:
-  //
+  [[nodiscard]] static Block encrypt( const Block& block, const RoundKeys& keys );
+  [[nodiscard]] static Block decrypt( const Block& block, const RoundKeys& keys );
 };
 
 }
