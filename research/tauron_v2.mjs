ce37713f9f84d5a5d9a1f5c83707c78e675e0382
komed3 <webmaster@komed3.de>
2026-08-09 13:35:17 +0200

    create block

--- research/tauron_v2.mjs
+++ research/tauron_v2.mjs
@@ -12,3 +12,15 @@ export const deriveKey = ( passphrase ) => {
   if ( typeof passphrase !== 'string' ) throw new TypeError( 'Passphrase must be a string' );
   return new Uint8Array( createHash( 'sha256' ).update( passphrase, 'utf8' ).digest().subarray( 0, KEY_SIZE ) );
 };
+
+const createBlock = ( data ) => {
+  if ( data.length > PAYLOAD_SIZE ) throw new RangeError( `Block payload exceeds ${ PAYLOAD_SIZE } bytes` );
+
+  const block = new Uint8Array( BLOCK_SIZE ), sum = checksum( data );
+
+  block[ 0 ] = data.length;
+  block.set( sum, LENGTH_SIZE );
+  block.set( data, LENGTH_SIZE + CHECKSUM_SIZE );
+
+  return block;
+};
