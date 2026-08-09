73df0575bd3ed3fc495ff3a754eb49cd1ac260a8
komed3 <webmaster@komed3.de>
2026-08-09 23:07:31 +0200

    declare block size

--- include/tauron/crypto/block_cipher.hpp
+++ include/tauron/crypto/block_cipher.hpp
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
 namespace tauron::crypto {
+
+inline constexpr std::size_t BLOCK_SIZE = 32;
+
+using Block = std::array< std::uint8_t, BLOCK_SIZE >;
 
 class BlockCipher {
 public:
