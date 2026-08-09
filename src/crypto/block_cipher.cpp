d789b0587f5b6a913151ddc449c47d58389f42dd
komed3 <webmaster@komed3.de>
2026-08-09 23:44:55 +0200

    define Words type

--- src/crypto/block_cipher.cpp
+++ src/crypto/block_cipher.cpp
@@ -1,10 +1,13 @@
 #include "tauron/crypto/block_cipher.hpp"
 
+#include <bit>
+
 namespace tauron::crypto {
 
 namespace {
 
-
+constexpr std::size_t WORDS = 8;
+using Words = std::array< std::uint32_t, WORDS >;
 
 } // namespace
 
