77248539c477fc3f05e680ab31f9b8bf08e9a85d
komed3 <webmaster@komed3.de>
2026-08-10 14:33:01 +0200

    generate salt + key 1

--- test/passphrase_test.cpp
+++ test/passphrase_test.cpp
@@ -7,4 +7,12 @@
 
 using namespace tauron::crypto;
 
-int main () {}
+int main () {
+  const std::string passphrase = "Tauron test passphrase";
+
+  const auto salt1 = NonceGenerator::generate();
+  const auto salt2 = NonceGenerator::generate();
+
+  const auto key1 = MasterKeyGenerator::generate( passphrase, salt1 );
+  const auto key1Again = MasterKeyGenerator::generate( passphrase, salt1 );
+}
