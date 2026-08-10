baef87ac61d0f004c8c46bc6aba1fb23d050667d
komed3 <webmaster@komed3.de>
2026-08-10 14:00:55 +0200

    create master key gen file

---
+++ include/tauron/crypto/master_key_generator.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <array>
+#include <cstdint>
+#include <string_view>
+
+#include "tauron/crypto/constants.hpp"
+
+namespace tauron::crypto {
+
+using MasterKey = std::array< std::uint8_t, KEY_SIZE >;
+using Salt = std::array< std::uint8_t, SALT_SIZE >;
+
+}
