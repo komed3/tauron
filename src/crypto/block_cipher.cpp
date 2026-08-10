c6381992d3635ef5e3c01d7217511a7523b1ece5
komed3 <webmaster@komed3.de>
2026-08-10 17:32:22 +0200

    Update block_cipher.cpp

--- src/crypto/block_cipher.cpp
+++ src/crypto/block_cipher.cpp
@@ -8,7 +8,15 @@ namespace tauron::crypto {
 
 namespace {
 
+constexpr std::uint32_t NONLINEAR_BASE = 0x9e3779b1;
 
+constexpr std::array< std::array< std::size_t, 2 >, 4 > PAIRS = {{
+  {{ 0, 1 }}, {{ 2, 3 }}, {{ 4, 5 }}, {{ 6, 7 }}
+}};
+
+constexpr std::array< std::array< std::size_t, 2 >, 4 > CROSS = {{
+  {{ 0, 2 }}, {{ 1, 3 }}, {{ 4, 6 }}, {{ 5, 7 }}
+}};
 
 } // namespace
 
