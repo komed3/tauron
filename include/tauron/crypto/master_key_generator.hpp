026d82d414c2bdb38d5948356b2e368c9afdc687
komed3 <webmaster@komed3.de>
2026-08-10 14:01:54 +0200

    declare MasterKeyGenerator cls

--- include/tauron/crypto/master_key_generator.hpp
+++ include/tauron/crypto/master_key_generator.hpp
@@ -11,4 +11,9 @@ namespace tauron::crypto {
 using MasterKey = std::array< std::uint8_t, KEY_SIZE >;
 using Salt = std::array< std::uint8_t, SALT_SIZE >;
 
+class MasterKeyGenerator {
+public:
+  static MasterKey generate( std::string_view passphrase, const Salt& salt );
+};
+
 }
