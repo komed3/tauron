1c601e48395e734c86b7e7fae3b95282313a6f16
komed3 <webmaster@komed3.de>
2026-08-10 11:49:31 +0200

    implement nonce generator using OS rng source

--- src/crypto/nonce.cpp
+++ src/crypto/nonce.cpp
@@ -14,3 +14,30 @@
 #include <sys/random.h>
 
 #endif
+
+namespace tauron::crypto {
+
+Nonce NonceGenerator::generate() {
+  Nonce nonce {};
+
+  #ifdef _WIN32
+    const auto status = BCryptGenRandom(
+      nullptr, nonce.data(), static_cast< ULONG >( nonce.size() ),
+      BCRYPT_USE_SYSTEM_PREFERRED_RNG
+    );
+
+    if ( status != 0 )
+      throw std::runtime_error( "Faild to generate cryptographically secure nonce" );
+
+  #else
+    const auto result = getrandom( nonce.data(), nonce.size(), 0 );
+
+    if ( result != static_cast< ssize_t >( nonce.size() ) )
+      throw std::runtime_error( "Faild to generate cryptographically secure nonce" );
+
+  #endif
+
+  return nonce;
+}
+
+}
