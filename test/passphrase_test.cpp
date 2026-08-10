183690797f504974c8de07ccee6f9c31852a85d4
komed3 <webmaster@komed3.de>
2026-08-10 14:31:24 +0200

    create passphrase -> master key test

---
+++ test/passphrase_test.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+#include "tauron/crypto/master_key_generator.hpp"
+#include "tauron/crypto/nonce_generator.hpp"
+
+using namespace tauron::crypto;
+
+int main () {}
