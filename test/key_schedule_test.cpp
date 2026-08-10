fead21738057bde963a3bdc5dcca7532a509a8f8
komed3 <webmaster@komed3.de>
2026-08-10 17:03:53 +0200

    generate keys

--- test/key_schedule_test.cpp
+++ test/key_schedule_test.cpp
@@ -6,7 +6,16 @@
 using namespace tauron::crypto;
 
 int main() {
+  const std::size_t rounds = 16;
+
   const Key key {};
   const auto nonce1 = NonceGenerator::generate();
   const auto nonce2 = NonceGenerator::generate();
+
+  const auto keys1 = KeySchedule::expand( key, nonce1, rounds );
+  const auto keys1Again = KeySchedule::expand( key, nonce1, rounds );
+  const auto keys2 = KeySchedule::expand( key, nonce2, rounds );
+
+  const bool sameNonceSameKeys = keys1 == keys1Again;
+  const bool differentNonceDifferentKeys = keys1 != keys2;
 }
