12ac937b8549f81e9440ec9dc18ff0b7a254c42e
komed3 <webmaster@komed3.de>
2026-08-10 18:01:42 +0200

    gen nonces, keys, cipher text

--- test/block_cipher_test.cpp
+++ test/block_cipher_test.cpp
@@ -9,6 +9,8 @@
 using namespace tauron::crypto;
 
 int main() {
+  const std::size_t rounds = 16;
+
   Key key {};
   Block block {};
 
@@ -17,4 +19,15 @@ int main() {
 
   for ( std::size_t i = 0; i < block.size(); ++i )
     block[ i ] = static_cast< std::uint8_t >( i + 32 );
+
+  const auto nonce1 = NonceGenerator::generate();
+  const auto nonce2 = NonceGenerator::generate();
+
+  const auto keys1 = KeySchedule::expand( key, nonce1, rounds );
+  const auto keys2 = KeySchedule::expand( key, nonce2, rounds );
+
+  const auto encrypted1 = BlockCipher::encrypt( block, keys1 );
+  const auto decrypted1 = BlockCipher::decrypt( encrypted1, keys1 );
+
+  const auto encrypted2 = BlockCipher::encrypt( block, keys2 );
 }
