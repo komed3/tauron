8d98b0b4b18223793f328ef40911d5af84129992
komed3 <webmaster@komed3.de>
2026-08-11 15:52:58 +0200

    calculate checksum

--- src/stream/checksum.cpp
+++ src/stream/checksum.cpp
@@ -5,6 +5,15 @@ namespace tauron::stream {
 std::uint16_t Checksum::calculate( std::span< const std::uint8_t > payload ) {
   std::uint16_t checksum = 0xFFFF;
 
+  for ( const auto byte : payload ) {
+    checksum ^= static_cast< std::uint16_t >( byte ) << 8;
+
+    for ( int i = 0; i < 8; ++i )
+      checksum = checksum & 0x8000
+        ? static_cast< std::uint16_t >( ( checksum << 1 ) ^ 0x1021 )
+        : static_cast< std::uint16_t >( checksum << 1 );
+  }
+
   return checksum;
 }
 
