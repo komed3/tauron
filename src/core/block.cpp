46c8cdc36a66ade75b0c048d5f31e7f567442f90
komed3 <webmaster@komed3.de>
2026-08-12 00:58:16 +0200

    create data block

--- src/core/block.cpp
+++ src/core/block.cpp
@@ -13,6 +13,9 @@ DataBlock Block::build( const std::uint8_t id, const std::span< const std::uint8
   if ( id == 0xFF && ! payload.empty() )
     throw std::invalid_argument( "Random block cannot contain payload" );
 
+  DataBlock block {};
+  block[ 0 ] = id;
+  block[ 1 ] = static_cast< std::uint8_t >( payload.size() );
 }
 
 ParsedBlock Block::parse( DataBlock block, std::uint8_t sequenceSize ) {}
