13a2cdb224bfc8133477e0547b8a6f3468dd0c42
komed3 <webmaster@komed3.de>
2026-08-12 01:30:44 +0200

    create + return parsed block

--- src/core/block.cpp
+++ src/core/block.cpp
@@ -31,6 +31,12 @@ DataBlock Block::build( const std::uint8_t id, const std::span< const std::uint8
   return block;
 }
 
-ParsedBlock Block::parse( DataBlock block, std::uint8_t sequenceSize ) {}
+ParsedBlock Block::parse( DataBlock block, std::uint8_t sequenceSize ) {
+  ParsedBlock result { .id = block[ 0 ], .payload = {}, .flag = BlockFlag::PASSED };
+  const auto id = block[ 0 ];
+  const auto length = block[ 1 ];
+
+  return result;
+}
 
 }
