6fc04925f608466156d9aa7dee58ac8cc36dd194
komed3 <webmaster@komed3.de>
2026-08-11 23:43:52 +0200

    add methods to generator nonce/salt directly

--- include/tauron/utils/random.hpp
+++ include/tauron/utils/random.hpp
@@ -1,14 +1,22 @@
 #pragma once
 
+#include <array>
 #include <cstddef>
 #include <cstdint>
 #include <span>
 
+#include "tauron/constants.hpp"
+
 namespace tauron::utils {
+
+using Nonce = std::array< std::uint8_t, NONCE_SIZE >;
+using Salt = std::array< std::uint8_t, SALT_SIZE >;
 
 class Random {
 public:
   static std::span< std::int8_t > generate( std::size_t length );
+  static Nonce nonce();
+  static Salt salt();
 };
 
 }
