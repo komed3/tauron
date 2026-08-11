6bf34fa2533f16c2780e0c0662b79ce57cb3a178
komed3 <webmaster@komed3.de>
2026-08-11 10:12:54 +0200

    create test file for crypto pipeline

---
+++ test/crypto_pipeline_test.cpp
@@ -0,0 +1,13 @@
+#include <array>
+#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+#include "tauron/crypto/block_cipher.hpp"
+#include "tauron/crypto/constants.hpp"
+#include "tauron/crypto/key_schedule.hpp"
+#include "tauron/crypto/master_key_generator.hpp"
+#include "tauron/crypto/nonce_generator.hpp"
+
+int main() {}
