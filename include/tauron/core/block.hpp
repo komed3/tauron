c30be9c0dc80886cd34287aa229f7eb94a426756
komed3 <webmaster@komed3.de>
2026-08-12 00:52:07 +0200

    declare parsed block struct + block class

--- include/tauron/core/block.hpp
+++ include/tauron/core/block.hpp
@@ -2,18 +2,32 @@
 
 #include <array>
 #include <cstdint>
+#include <span>
+#include <vector>
 
 #include "tauron/constants.hpp"
 
 namespace tauron::core {
 
-using Block = std::array< std::int8_t, BLOCK_SIZE >;
+using DataBlock = std::array< std::int8_t, BLOCK_SIZE >;
 
 enum class BlockFlag {
   PASSED,
   INVALID_ID,
   INVALID_CHECKSUM,
   INVALID_LENGTH
+};
+
+struct ParsedBlock {
+  std::uint8_t id;
+  std::vector< std::uint8_t > payload;
+  BlockFlag flag;
+};
+
+class Block {
+public:
+  static DataBlock build( const std::uint8_t id, const std::span< const std::uint8_t > payload );
+  static ParsedBlock parse( DataBlock block, std::uint8_t sequenceSize );
 };
 
 }
