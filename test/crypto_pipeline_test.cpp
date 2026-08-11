d24fa5a3e0d560bb148fe5597eb9ddacb651c5da
komed3 <webmaster@komed3.de>
2026-08-11 10:16:04 +0200

    print HEX

--- test/crypto_pipeline_test.cpp
+++ test/crypto_pipeline_test.cpp
@@ -12,4 +12,16 @@
 
 using namespace tauron::crypto;
 
+namespace {
+
+void printHex( const auto& data ) {
+  for ( const auto byte : data )
+    std::cout << std::hex << std::setw( 2 ) << std::setfill( '0' )
+              << static_cast< unsigned int >( byte ) << " ";
+
+  std::cout << std::dec << "\n";
+}
+
+}
+
 int main() {}
