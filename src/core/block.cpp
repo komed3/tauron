ea5e8e1f50693c9ee481d8e70d722234990b8ed6
komed3 <webmaster@komed3.de>
2026-08-12 00:53:52 +0200

    prep block methods

--- src/core/block.cpp
+++ src/core/block.cpp
@@ -1,7 +1,9 @@
 #include "tauron/core/block.hpp"
 
-namespace tauron::block {
+namespace tauron::core {
 
+DataBlock Block::build( const std::uint8_t id, const std::span< const std::uint8_t > payload ) {}
 
+ParsedBlock Block::parse( DataBlock block, std::uint8_t sequenceSize ) {}
 
 }
