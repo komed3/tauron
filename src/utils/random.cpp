54e028ea3382517ab98da21bb49e573745b2cf87
komed3 <webmaster@komed3.de>
2026-08-11 23:47:48 +0200

    include header

--- src/utils/random.cpp
+++ src/utils/random.cpp
@@ -1,5 +1,18 @@
 #include "tauron/utils/random.hpp"
 
+#include <stdexcept>
+
+#ifdef _WIN32
+
+#include <windows.h>
+#include <bcrypt.h>
+
+#else
+
+#include <sys/random.h>
+
+#endif
+
 namespace tauron::utils {
 
 std::span< std::uint8_t > Random::generate( std::size_t length ) {}
