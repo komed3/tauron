edef5b9369ce8d9c764a7dcb7a8ad3f41f7618e4
komed3 <webmaster@komed3.de>
2026-08-11 23:26:19 +0200

    restart implementation

---
+++ include/tauron/core/constants.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstddef>
+
+namespace tauron::core {
+
+inline constexpr std::size_t BLOCK_SIZE = 32;
+inline constexpr std::size_t BLOCK_PAYLOAD = 28;
+
+inline constexpr std::size_t KEY_SIZE = 32;
+
+}
