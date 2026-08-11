1a0680c48ff819c240dbc05b13f5bafc0eed698c
komed3 <webmaster@komed3.de>
2026-08-12 00:44:01 +0200

    implement checksum utility

---
+++ src/utils/checksum.cpp
@@ -0,0 +1,20 @@
+#include "tauron/utils/checksum.hpp"
+
+namespace tauron::utils {
+
+std::uint16_t Checksum::calculate( std::span< const std::uint8_t > payload ) {
+  std::uint16_t checksum = 0xFFFF;
+
+  for ( const auto byte : payload ) {
+    checksum ^= static_cast< std::uint16_t >( byte ) << 8;
+
+    for ( int i = 0; i < 8; ++i )
+      checksum = checksum & 0x8000
+        ? static_cast< std::uint16_t >( ( checksum << 1 ) ^ 0x1021 )
+        : static_cast< std::uint16_t >( checksum << 1 );
+  }
+
+  return checksum;
+}
+
+}
