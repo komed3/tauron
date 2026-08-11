509512e6c0d2929795d1ab1ddd0a6c17d10dad98
komed3 <webmaster@komed3.de>
2026-08-12 01:18:46 +0200

    Update checksum.cpp

--- src/utils/checksum.cpp
+++ src/utils/checksum.cpp
@@ -1,5 +1,7 @@
 #include "tauron/utils/checksum.hpp"
 
+#include <cstddef>
+
 namespace tauron::utils {
 
 std::uint16_t Checksum::calculate( std::span< const std::uint8_t > payload ) {
@@ -8,7 +10,7 @@ std::uint16_t Checksum::calculate( std::span< const std::uint8_t > payload ) {
   for ( const auto byte : payload ) {
     checksum ^= static_cast< std::uint16_t >( byte ) << 8;
 
-    for ( int i = 0; i < 8; ++i )
+    for ( std::size_t i = 0; i < 8; ++i )
       checksum = checksum & 0x8000
         ? static_cast< std::uint16_t >( ( checksum << 1 ) ^ 0x1021 )
         : static_cast< std::uint16_t >( checksum << 1 );
