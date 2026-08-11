947014fc974dde95e9758adbfcf1d1c5f4dc6de4
komed3 <webmaster@komed3.de>
2026-08-11 16:27:21 +0200

    declare block API

--- include/tauron/stream/block.hpp
+++ include/tauron/stream/block.hpp
@@ -1,20 +1,30 @@
 #pragma once
 
 #include <cstdint>
+#include <span>
 #include <vector>
+
+#include "tauron/crypto/block_cipher.hpp"
 
 namespace tauron::stream {
 
 enum class BlockFlag {
   PASSED,
-  INVALID_LENGTH,
-  INVALID_CHECKSUM
+  INVALID_ID,
+  INVALID_CHECKSUM,
+  INVALID_LENGTH
 };
 
 struct ParsedBlock {
   std::uint8_t id;
   std::vector< std::uint8_t > payload;
   BlockFlag flag;
+};
+
+class Block {
+public:
+  static crypto::Block build( std::uint8_t id, std::span< const std::uint8_t > payload );
+  static ParsedBlock parse( crypto::Block block, std::uint8_t sequenceSize );
 };
 
 }
