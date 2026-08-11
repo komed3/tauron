7c11d473d8a79e17dcb97684a2d1791a93ece1a4
komed3 <webmaster@komed3.de>
2026-08-11 16:35:20 +0200

    Create block.cpp

---
+++ src/stream/block.cpp
@@ -0,0 +1,13 @@
+#include "tauron/stream/block.hpp"
+
+#include <stdexcept>
+
+#include "tauron/crypto/nonce_generator.hpp"
+#include "tauron/stream/checksum.hpp"
+#include "tauron/stream/constants.hpp"
+
+namespace tauron::stream {
+
+
+
+}
