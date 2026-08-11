af26c5ffab80a2acf8a02853da7049bb83508c1b
komed3 <webmaster@komed3.de>
2026-08-11 16:13:51 +0200

    declare block flag

--- include/tauron/stream/block.hpp
+++ include/tauron/stream/block.hpp
@@ -5,10 +5,16 @@
 
 namespace tauron::stream {
 
+enum class BlockFlag {
+  PASSED,
+  INVALID_LENGTH,
+  INVALID_CHECKSUM
+};
+
 struct ParsedBlock {
   std::uint8_t id;
   std::vector< std::uint8_t > payload;
-  bool valid;
+  BlockFlag flag;
 };
 
 }
