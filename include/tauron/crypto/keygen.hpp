e3c4ef5273a2b118f0cfb75ebace95fb1a74213e
komed3 <webmaster@komed3.de>
2026-08-12 11:52:55 +0200

    declare round keys as vector of words

--- include/tauron/crypto/keygen.hpp
+++ include/tauron/crypto/keygen.hpp
@@ -3,13 +3,16 @@
 #include <array>
 #include <cstdint>
 #include <string_view>
+#include <vector>
 
 #include "tauron/core/constants.hpp"
 #include "tauron/utils/random.hpp"
+#include "tauron/utils/word.hpp"
 
 namespace tauron::crypto {
 
 using Key = std::array< std::uint8_t, core::KEY_SIZE >;
+using RoundKeys = std::vector< utils::Words >;
 
 class KeyGen {
 public:
