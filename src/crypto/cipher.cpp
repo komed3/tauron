040d605d9b68ae6bf25f5ca7d85e9acc5381b603
komed3 <webmaster@komed3.de>
2026-08-12 19:03:38 +0200

    implement rotation methods

--- src/crypto/cipher.cpp
+++ src/crypto/cipher.cpp
@@ -18,6 +18,38 @@ constexpr std::uint32_t inverse32( std::uint32_t value ) noexcept {
   return result;
 }
 
+constexpr unsigned injectionRotation( std::size_t round, std::size_t index ) noexcept {
+  return ( round * 7 + index * 3 ) & 31;
+}
+
+constexpr unsigned diffusionRotationA( std::size_t round, std::size_t index ) noexcept {
+  return ( round * 5 + index * 7 + 3 ) & 31;
+}
+
+constexpr unsigned diffusionRotationB( std::size_t round, std::size_t index ) noexcept {
+  return ( round * 3 + index * 11 + 5 ) & 31;
+}
+
+constexpr unsigned butterflyRotationA( std::size_t round, std::size_t index ) noexcept {
+  return ( round * 3 + index * 5 + 7 ) & 31;
+}
+
+constexpr unsigned butterflyRotationB( std::size_t round, std::size_t index ) noexcept {
+  return ( round * 7 + index * 3 + 11 ) & 31;
+}
+
+constexpr unsigned crossRotationA( std::size_t round, std::size_t index ) noexcept {
+  return ( round * 11 + index * 3 + 5 ) & 31;
+}
+
+constexpr unsigned crossRotationB( std::size_t round, std::size_t index ) noexcept {
+  return ( round * 13 + index * 7 + 9 ) & 31;
+}
+
+constexpr std::uint32_t multiplier( std::size_t round, std::size_t index ) noexcept {
+  return NONLINEAR_BASE + ( round + index ) * 2;
+}
+
 } // namespace
 
 core::Block Cipher::encrypt( const core::Block& block, const RoundKeys& keys ) noexcept {}
