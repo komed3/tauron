86f5dcd5714fece83c82524a99709bfc72eed842
komed3 <webmaster@komed3.de>
2026-08-12 23:11:04 +0200

    generate round keys

--- test/cipher_perf.cpp
+++ test/cipher_perf.cpp
@@ -5,13 +5,23 @@
 #include <vector>
 
 #include "tauron/crypto/cipher.hpp"
+#include "tauron/crypto/keygen.hpp"
+#include "tauron/utils/random.hpp"
 
 using namespace tauron::crypto;
+using namespace tauron::utils;
+
 using Clock = std::chrono::steady_clock;
+
+constexpr std::size_t BLOCKS = 10'000'000;
+constexpr std::size_t ROUNDS = 16;
 
 int main() {
   Key key {};
 
   for ( std::size_t i = 0; i < key.size(); ++i )
     key[ i ] = static_cast< std::uint8_t >( i );
+
+  const auto nonce = Random::nonce();
+  const auto keys = KeyGen::expand( key, nonce, ROUNDS );
 }
