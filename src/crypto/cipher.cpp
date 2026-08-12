d627afeed80160d1b74e877f2a73f69e04c2df25
komed3 <webmaster@komed3.de>
2026-08-12 18:39:18 +0200

    prepare cipher API

--- src/crypto/cipher.cpp
+++ src/crypto/cipher.cpp
@@ -2,6 +2,10 @@
 
 namespace tauron::crypto {
 
+namespace {} // namespace
 
+core::Block Cipher::encrypt( const core::Block& block, const RoundKeys& keys ) noexcept {}
+
+core::Block Cipher::decrypt( const core::Block& block, const RoundKeys& keys ) noexcept {}
 
 }
