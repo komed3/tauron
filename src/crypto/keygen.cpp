d4eeb9694a287d4d650b582350ca359a9ff86897
komed3 <webmaster@komed3.de>
2026-08-12 12:01:06 +0200

    derive master key from passphrase using libsodium's Agron2ID

---
+++ src/crypto/keygen.cpp
@@ -0,0 +1,19 @@
+#include "tauron/crypto/keygen.hpp"
+
+#include <sodium.h>
+#include <stdexcept>
+
+namespace tauron::crypto {
+
+Key KeyGen::derive( std::string_view passphrase, const utils::Salt& salt ) {
+  Key key {};
+
+  auto result = crypto_pwhash(
+    key.data(), key.size(), passphrase.data(), passphrase.size(), salt.data(),
+    crypto_pwhash_OPSLIMIT_MODERATE, crypto_pwhash_MEMLIMIT_MODERATE, crypto_pwhash_ALG_ARGON2ID13
+  );
+
+  return key;
+}
+
+}
