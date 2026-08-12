8b34a6dc087b4dd7e11803a108b1394c8080bfc6
komed3 <webmaster@komed3.de>
2026-08-12 11:49:16 +0200

    convert bytes to word by given offset

--- include/tauron/utils/word.hpp
+++ include/tauron/utils/word.hpp
@@ -11,4 +11,12 @@ namespace tauron::utils {
 using Words = std::array< std::uint32_t, core::WORDS >;
 using Bytes = std::array< std::uint8_t, core::BLOCK_SIZE >;
 
+inline constexpr std::uint32_t toWord( const Bytes& bytes, std::size_t offset ) noexcept {
+  return
+    ( static_cast< std::uint32_t >( bytes[ offset ] ) ) |
+    ( static_cast< std::uint32_t >( bytes[ offset + 1 ] ) <<  8 ) |
+    ( static_cast< std::uint32_t >( bytes[ offset + 2 ] ) << 16 ) |
+    ( static_cast< std::uint32_t >( bytes[ offset + 3 ] ) << 24 );
+}
+
 }
