11b59626c7be2e21e771b3b8eed7b6473e1ad051
komed3 <webmaster@komed3.de>
2026-08-12 12:49:37 +0200

    declare permut utility header

--- include/tauron/utils/permutation.hpp
+++ include/tauron/utils/permutation.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <cstddef>
+#include <cstdint>
+#include <span>
+#include <vector>
+
 namespace tauron::utils {
 
-
+class Permutation {
+public:
+  static std::vector< std::size_t > generate( std::span< const std::uint8_t> context, std::size_t size );
+};
 
 }
