4db53c37e4337ad4824b97856e621c02a59b72d9
komed3 <webmaster@komed3.de>
2026-08-12 18:57:43 +0200

    declare consts

--- src/crypto/cipher.cpp
+++ src/crypto/cipher.cpp
@@ -2,7 +2,16 @@
 
 namespace tauron::crypto {
 
-namespace {} // namespace
+namespace {
+
+using Pair = std::array< std::size_t, 2 >;
+
+constexpr std::array< Pair, 4 > PAIRS = {{ { 0, 1 }, { 2, 3 }, { 4, 5 }, { 6, 7 } }};
+constexpr std::array< Pair, 4 > CROSS = {{ { 0, 2 }, { 1, 3 }, { 4, 6 }, { 5, 7 } }};
+
+constexpr std::uint32_t NONLINEAR_BASE = 0x9e3779b1;
+
+} // namespace
 
 core::Block Cipher::encrypt( const core::Block& block, const RoundKeys& keys ) noexcept {}
 
