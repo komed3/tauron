588817451a636ae251ec5bd2feb27d1bdab9bac7
komed3 <webmaster@komed3.de>
2026-08-12 12:59:29 +0200

    declare inline constexpr constants

--- src/utils/permutation.cpp
+++ src/utils/permutation.cpp
@@ -7,11 +7,14 @@ namespace tauron::utils {
 
 namespace {
 
+inline constexpr std::uint64_t MIX_CONSTANT_A = 0xbf58476d1ce4e5b9ULL;
+inline constexpr std::uint64_t MIX_CONSTANT_B = 0x94d049bb133111ebULL;
+
 std::uint64_t mix( std::uint64_t value ) noexcept {
   value ^= value >> 30;
-  value *= 0xbf58476d1ce4e5b9ULL;
+  value *= MIX_CONSTANT_A;
   value ^= value >> 27;
-  value *= 0x94d049bb133111ebULL;
+  value *= MIX_CONSTANT_B;
   value ^= value >> 31;
 
   return value;
