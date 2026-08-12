c5695a40d0f2519dcd780176087d639b06cf9ff7
komed3 <webmaster@komed3.de>
2026-08-12 18:59:41 +0200

    implement inverse32

--- src/crypto/cipher.cpp
+++ src/crypto/cipher.cpp
@@ -11,6 +11,13 @@ constexpr std::array< Pair, 4 > CROSS = {{ { 0, 2 }, { 1, 3 }, { 4, 6 }, { 5, 7 
 
 constexpr std::uint32_t NONLINEAR_BASE = 0x9e3779b1;
 
+constexpr std::uint32_t inverse32( std::uint32_t value ) noexcept {
+  auto result = value;
+  for ( int i = 0; i < 5; ++i ) result *= 2 - value * result;
+
+  return result;
+}
+
 } // namespace
 
 core::Block Cipher::encrypt( const core::Block& block, const RoundKeys& keys ) noexcept {}
