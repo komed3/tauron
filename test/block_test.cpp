1d4a0daee6cf8d68356678d5b3d9609bfb99aca8
komed3 <webmaster@komed3.de>
2026-08-15 16:26:39 +0200

    update test files

--- test/block_test.cpp
+++ test/block_test.cpp
@@ -13,7 +13,7 @@ static void printResult( const char* name, const bool passed ) {
   std::cout << "[" << ( passed ? "PASS" : "FAIL" ) << "] " << name << '\n';
 }
 
-void printHex( const auto& data ) {
+static void printHex( const auto& data ) {
   for ( const auto byte : data )
     std::cout << std::hex << std::setw( 2 ) << std::setfill( '0' )
               << static_cast< unsigned int >( byte ) << " ";
