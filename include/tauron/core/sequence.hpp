09ce0f400a280981b9e6ccbcf7151832563edfc5
komed3 <webmaster@komed3.de>
2026-08-15 12:01:12 +0200

    manage includes

--- include/tauron/core/sequence.hpp
+++ include/tauron/core/sequence.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <array>
+#include <cstddef>
+#include <span>
+
+#include "tauron/core/block.hpp"
+#include "tauron/core/constants.hpp"
+
 namespace tauron::core {
 
 class Sequence {
