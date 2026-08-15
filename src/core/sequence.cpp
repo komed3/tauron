0a6f9bb481c35861efc0f986bd450a94488f537b
komed3 <webmaster@komed3.de>
2026-08-15 12:19:55 +0200

    get random context for permutation

--- src/core/sequence.cpp
+++ src/core/sequence.cpp
@@ -1,5 +1,8 @@
 #include "tauron/core/sequence.hpp"
 
+#include "tauron/utils/permutation.hpp"
+#include "tauron/utils/random.hpp"
+
 namespace tauron::core {
 
 SequenceResult Sequence::build( std::span< const std::uint8_t > payload, bool eof ) {
@@ -9,6 +12,9 @@ SequenceResult Sequence::build( std::span< const std::uint8_t > payload, bool eo
   const auto count = eof
     ? ( payload.size() + BLOCK_PAYLOAD - 1 ) / BLOCK_PAYLOAD
     : SEQ_BLOCKS;
+
+  std::array< std::uint8_t, BLOCK_SIZE > context {};
+  utils::Random::fill( context );
 
   return result;
 }
