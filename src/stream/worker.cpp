23a04346b8b014398726b749014f17a3bc58a9a1
komed3 <webmaster@komed3.de>
2026-08-16 10:56:48 +0200

    init worker cls

--- src/stream/worker.cpp
+++ src/stream/worker.cpp
@@ -2,6 +2,8 @@
 
 namespace tauron::stream {
 
-
+Worker::Worker( WorkerId id, const crypto::RoundKeys& keys ) :
+  id_( id ), keys_( keys ), state_( WorkerState::IDLE ),
+  processed_( 0 ), written_( 0 ), time_( Clock::now() ) {};
 
 }
