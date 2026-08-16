d920b231d36bf0d4c02888825c0b149ebbafca9a
komed3 <webmaster@komed3.de>
2026-08-16 11:09:49 +0200

    return failed if worker gets more then one run() request

--- src/stream/worker.cpp
+++ src/stream/worker.cpp
@@ -1,10 +1,20 @@
 #include "tauron/stream/worker.hpp"
 
+#include <stdexcept>
+
 namespace tauron::stream {
 
 Worker::Worker( WorkerId id, const crypto::RoundKeys& keys ) :
   id_( id ), keys_( keys ), state_( WorkerState::IDLE ),
-  processed_( 0 ), written_( 0 ), time_( Clock::now() ) {};
+  processed_( 0 ), written_( 0 ), time_( Clock::now() ) {}
+
+WorkerResult Worker::run(
+  Operation operation, std::span< const std::uint8_t > payload,
+  std::span< std::uint8_t > output, bool eof
+) {
+  if ( state_ != WorkerState::IDLE )
+    return { WorkerResultState::FAILED, 0, 0 };
+}
 
 void Worker::stop() {
   if ( state_ == WorkerState::PROCESSING )
