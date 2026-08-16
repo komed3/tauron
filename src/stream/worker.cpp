324c8ebb16c7b07ad4eac04a88496051f3a514e6
komed3 <webmaster@komed3.de>
2026-08-16 03:11:03 +0200

    implement worker cls constructor

--- src/stream/worker.cpp
+++ src/stream/worker.cpp
@@ -8,4 +8,8 @@ namespace {
 
 } // namespace
 
+Worker::Worker( WorkerId id, Operation operation, const crypto::RoundKeys& keys ) :
+  id( id ), operation( operation ), keys( keys ), state( WorkerState::IDLE ),
+  processed( 0 ), written( 0 ), time_point( Clock::now() ) {}
+
 }
