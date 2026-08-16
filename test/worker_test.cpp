0ad24e585a6ba2f10d36b79048c718d4d3f6872d
komed3 <webmaster@komed3.de>
2026-08-16 13:52:47 +0200

    Create worker_test.cpp

---
+++ test/worker_test.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include "tauron/core/constants.hpp"
+#include "tauron/crypto/keygen.hpp"
+#include "tauron/stream/worker.hpp"
+#include "tauron/utils/random.hpp"
+
+using namespace tauron::crypto;
+using namespace tauron::stream;
+using namespace tauron::utils;
+
+int main() {
+  return 0;
+}
