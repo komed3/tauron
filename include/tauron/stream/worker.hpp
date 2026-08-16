6877ba18604cef6b8b33db70f36cc411c6dd66f8
komed3 <webmaster@komed3.de>
2026-08-16 03:02:08 +0200

    add stats

--- include/tauron/stream/worker.hpp
+++ include/tauron/stream/worker.hpp
@@ -10,6 +10,7 @@
 namespace tauron::stream {
 
 using WorkerId = std::uint8_t;
+using Time = std::chrono::steady_clock::time_point;
 
 enum class Operation {
   ENCRYPT,
@@ -42,13 +43,20 @@ public:
 
   WorkerId id() const;
   Operation operation() const;
-  WorkerState state() const {};
+  WorkerState state() const;
+
+  std::size_t bytes_processed() const;
+  std::size_t bytes_written() const;
+  Time last_progress() const;
 
 private:
   WorkerId id;
   Operation operation;
   crypto::RoundKeys keys;
   WorkerState state;
+  std::size_t processed;
+  std::size_t written;
+  Time time_point;
 };
 
 }
