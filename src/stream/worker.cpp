99d23be7280b7fe6bdb93198e7a49b411f0f05eb
komed3 <webmaster@komed3.de>
2026-08-16 10:57:53 +0200

    cancel worker

--- src/stream/worker.cpp
+++ src/stream/worker.cpp
@@ -6,4 +6,9 @@ Worker::Worker( WorkerId id, const crypto::RoundKeys& keys ) :
   id_( id ), keys_( keys ), state_( WorkerState::IDLE ),
   processed_( 0 ), written_( 0 ), time_( Clock::now() ) {};
 
+void Worker::stop() {
+  if ( state_ == WorkerState::PROCESSING )
+    state_ = WorkerState::CANCELLED;
+}
+
 }
