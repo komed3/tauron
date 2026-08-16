8c0745a914ef1e08b803c2615b443dba60306f63
komed3 <webmaster@komed3.de>
2026-08-16 02:28:04 +0200

    declare worker operation enum

--- include/tauron/stream/worker.hpp
+++ include/tauron/stream/worker.hpp
@@ -6,9 +6,14 @@ namespace tauron::stream {
 
 using WorkerId = uint8_t;
 
+enum class Operation {
+  ENCRYPT,
+  DECRYPT
+};
+
 class Worker {
 public:
-  explicit Worker( WorkerId id );
+  explicit Worker( WorkerId id, Operation operation );
 };
 
 }
